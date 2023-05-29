e80a3c0375db257e6e3ea66726636f46849e665f
hpweisberg <hpweisberg@gmail.com>
2023-05-29 13:44:59 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+	'env': {
+		'es2021': true,
+		'node': true,
+		'jest': true,
+	},
+	'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		// 'project': './tsconfig.json',
+		'ecmaVersion': 'latest',
+		'sourceType': 'module',
+		'ecmaFeatures': {
+			'jsx': true
+		}
+	},
+	'plugins': [
+		'react',
+		'react-hooks',
+		'react-native',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			2,
+			{SwitchCase: 1}
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
+
+
+
